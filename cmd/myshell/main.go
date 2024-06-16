58b03145d4d39b563ca7eaf1400b620aee140895
01zulfi <85733202+01zulfi@users.noreply.github.com>
2024-06-16 17:39:00 +0500

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,10 +12,28 @@ type Input struct {
 	arguments []string
 }
 
-func main() {
+type Shell struct {
+	commands map[string]string
+}
+
+func (s *Shell) isBuiltin(command string) bool {
+	_, ok := s.commands[command]
+	return ok
+}
+
+func newShell() Shell {
 	commands := make(map[string]string)
 	commands["exit"] = "exit"
 	commands["echo"] = "echo"
+	commands["type"] = "type"
+	shell := Shell{
+		commands: commands,
+	}
+	return shell
+}
+
+func main() {
+	shell := newShell()
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -32,10 +50,20 @@ func main() {
 		}
 
 		switch input.command {
-		case commands["exit"]:
+		case shell.commands["exit"]:
 			os.Exit(0)
-		case commands["echo"]:
+		case shell.commands["echo"]:
 			fmt.Fprint(os.Stdout, strings.Join(input.arguments, " "))
+		case shell.commands["type"]:
+			argument := strings.Join(input.arguments, " ")
+			isBuiltin := shell.isBuiltin(argument)
+			var out string
+			if isBuiltin {
+				out = fmt.Sprintf("%s is a shell builtin", argument)
+			} else {
+				out = fmt.Sprintf("%s: not found", argument)
+			}
+			fmt.Fprint(os.Stdout, out)
 		default:
 			out := commandNotFoundMesssage(input.command)
 			fmt.Fprint(os.Stdout, out)
