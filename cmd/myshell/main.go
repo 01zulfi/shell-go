54635a201811e97aebc3030f4a2f576922ce3d43
01zulfi <85733202+01zulfi@users.noreply.github.com>
2024-06-16 12:21:16 +0500

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,6 +9,17 @@ import (
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error while reading command: ", err)
+		return
+	}
+
+	out := commandNotFoundMesssage(input[:len(input)-1])
+	fmt.Fprint(os.Stdout, out)
+}
+
+func commandNotFoundMesssage(cmd string) string {
+	return fmt.Sprintf("%s: command not found", cmd)
 }
